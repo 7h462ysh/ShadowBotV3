48de4c553796398ce681a0041266192c94c33168
7h462ysh <102866765+7h462ysh@users.noreply.github.com>
2022-04-10 21:52:57 -0300

    Update No etiquetar2.js

--- plugins/No etiquetar2.js
+++ plugins/No etiquetar2.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-*[ ⚠ ️] No etiquetes a mi creadora, si es algo urgente contacta con ella a su chat privado*
+*[ ⚠ ️] No etiquetes a mi creador/a, si es algo urgente contacta con el/ella a su chat privado*
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@51940236711/i
+handler.customPrefix = /@51940236711 , @5493491456806/i
 handler.command = new RegExp
 
 module.exports = handler
