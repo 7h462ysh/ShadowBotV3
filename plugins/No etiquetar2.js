3cd550b5280a36079cd5a12f1d1fa8eec7b02828
7h462ysh <102866765+7h462ysh@users.noreply.github.com>
2022-04-11 09:03:46 -0300

    Update No etiquetar2.js

--- plugins/No etiquetar2.js
+++ plugins/No etiquetar2.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@51940236711 , @5493491456806/i
+handler.customPrefix = /@51940236711/@5493491456806/i
 handler.command = new RegExp
 
 module.exports = handler
